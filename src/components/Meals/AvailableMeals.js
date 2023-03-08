0934e97ab176cb05c4766eb324e01b0e4299d312
hexfloo <shoham.palmer@gmail.com>
2023-03-08 10:05:46 +0200

    Connect to DB to fetch meals

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,7 +1,8 @@
+import { useState, useEffect, useCallback } from "react";
 import classes from "./AvailableMeals.module.css";
 import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
-const DUMMY_MEALS = [
+/* const DUMMY_MEALS = [
   {
     id: "m1",
     name: "Sushi",
@@ -11,7 +12,7 @@ const DUMMY_MEALS = [
   {
     id: "m2",
     name: "Rainbow Panzanella Salad",
-    description: "Colorful salad with a dash of toasted bread",
+    description: "Colorful salad with toasted bread",
     price: 19.5,
   },
   {
@@ -26,10 +27,51 @@ const DUMMY_MEALS = [
     description: "Healthy...and green...",
     price: 18.99,
   },
-];
+]; */
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState(null);
+
+  const fetchMealsHandler = useCallback(async () => {
+    setHttpError(false);
+    try {
+      const response = await fetch(
+        "https://react-meals-hexfloo-default-rtdb.firebaseio.com/meals.json",
+        { method: "GET" }
+      );
+      if (!response.ok) {
+        throw new Error("Fetch went wrong"); // this error will be handled in catch{}
+      }
+      const data = await response.json();
+      const loadedMeals = []; // transforming the data recieved from firebase (object) to an array of objects.
+      for (const key in data) {
+        loadedMeals.push({
+          id: key,
+          name: data[key].name,
+          description: data[key].description,
+          price: data[key].price,
+          // this is based on how the data is saved in the db. The key is the id of the item and it's value is an object with the rest of the data.
+        });
+      }
+      setMeals(loadedMeals);
+    } catch (err) {
+      setIsLoading(false); //so error message will show
+      setHttpError(err.message);
+    } finally {
+      setIsLoading(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchMealsHandler().catch((err) => {
+      setIsLoading(false); //so error message will show
+      setHttpError(err.message);
+    });
+  }, [fetchMealsHandler]);
+
+  const mealsList = meals.map((meal) => (
     <MealItem
       id={meal.id}
       key={meal.id}
@@ -41,6 +83,8 @@ const AvailableMeals = () => {
   return (
     <section className={classes.meals}>
       <Card>
+        {httpError && <h3 style={{ textAlign: "center" }}>{httpError}</h3>}
+        {isLoading && <h3 style={{ textAlign: "center" }}>Loading meals...</h3>}
         <ul>{mealsList}</ul>
       </Card>
     </section>
